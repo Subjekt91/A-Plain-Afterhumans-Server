952508b04ee694b27061d3ec12df20fb845f8ead
Subjekt_91 <subjekt_91@gmx.de>
2017-10-04 22:19:41 +0200

    Int Commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#High Charity Servers By Subjekt_91
+
+java -server -Xmx4g -XX:ParallelGCThreads=2 -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -Dfml.debugExit=true -jar server.jar nogui
+
